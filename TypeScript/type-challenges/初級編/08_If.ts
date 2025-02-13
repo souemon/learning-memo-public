ce9157a6abe5598f297540d2f799bca560bdea98
souemon <kobekobekitakita@gmail.com>
2025-02-13 21:11:25 +0900

    easy_08

---
+++ TypeScript/type-challenges/初級編/08_If.ts
@@ -0,0 +1,4 @@
+// 出典：https://github.com/type-challenges/type-challenges/blob/main/questions/00268-easy-if/README.md
+// 出典：https://zenn.dev/yskn_sid25/books/type-challenges/viewer/chapter1#if
+
+type If<C extends boolean, T, F> = C extends true ? T : F;
