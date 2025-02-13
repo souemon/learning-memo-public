59abe34c9e1c8a3e70dc84e1b56fbf6e1547de92
souemon <kobekobekitakita@gmail.com>
2025-02-13 21:57:45 +0900

    easy_13

---
+++ TypeScript/type-challenges/初級編/13_Parameters.ts
@@ -0,0 +1,8 @@
+// 出典：https://github.com/type-challenges/type-challenges/blob/main/questions/03312-easy-parameters/README.ja.md
+// 出典：https://zenn.dev/yskn_sid25/books/type-challenges/viewer/chapter1#parameters
+
+type MyParameters<T extends (...args: any[]) => any> = T extends (
+  ...args: infer R
+) => any
+  ? R
+  : never;
