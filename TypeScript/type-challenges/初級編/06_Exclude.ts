2d9ae66e41352210b42ad2d4d5ac79a6486888cc
souemon <kobekobekitakita@gmail.com>
2025-02-13 20:53:19 +0900

    easy_06

---
+++ TypeScript/type-challenges/初級編/06_Exclude.ts
@@ -0,0 +1,4 @@
+// 出典：https://github.com/type-challenges/type-challenges/blob/main/questions/00043-easy-exclude/README.ja.md
+// 出典：https://zenn.dev/yskn_sid25/books/type-challenges/viewer/chapter1#exclude
+
+type MyExclude<T, U> = T extends U ? never : T;
