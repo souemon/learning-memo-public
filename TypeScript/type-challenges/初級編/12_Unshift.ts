aae50d59db49c36ccee1a903981b86298f8cac07
souemon <kobekobekitakita@gmail.com>
2025-02-13 21:57:39 +0900

    easy_12

---
+++ TypeScript/type-challenges/初級編/12_Unshift.ts
@@ -0,0 +1,4 @@
+// 出典：https://github.com/type-challenges/type-challenges/blob/main/questions/03060-easy-unshift/README.ja.md
+// 出典：https://zenn.dev/yskn_sid25/books/type-challenges/viewer/chapter1#unshift
+
+type Unshift<T extends any[], U> = [U, ...T];
