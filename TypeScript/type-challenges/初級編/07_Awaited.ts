b87c0cd7e022ca4b69161542def4eeda006b9f4c
souemon <kobekobekitakita@gmail.com>
2025-02-13 21:11:19 +0900

    easy_07

--- TypeScript/type-challenges/初級編/07_Awaited.ts
+++ TypeScript/type-challenges/初級編/07_Awaited.ts
@@ -1,4 +1,4 @@
 // 出典：https://github.com/type-challenges/type-challenges/blob/main/questions/00189-easy-awaited/README.ja.md
 // 出典：https://zenn.dev/yskn_sid25/books/type-challenges/viewer/chapter1#awaited
 
-type MyExclude<T, U> = T extends U ? never : T;
+type MyAwaited<T> = T extends PromiseLike<infer U> ? MyAwaited<U> : T;
