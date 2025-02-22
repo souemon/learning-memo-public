4fd093e21ddfbe56806f6fcda5136f9b9ae8e5c3
souemon <kobekobekitakita@gmail.com>
2025-02-22 15:47:15 +0900

    medium_05

---
+++ TypeScript/type-challenges/中級編/05_Tuple to Union.ts
@@ -0,0 +1,4 @@
+// 出典：https://github.com/type-challenges/type-challenges/blob/main/questions/00010-medium-tuple-to-union/README.ja.md
+// 出典：https://zenn.dev/yskn_sid25/books/type-challenges/viewer/chapter2#tuple-to-union
+
+type TupleToUnion<T extends any[]> = T[number];
