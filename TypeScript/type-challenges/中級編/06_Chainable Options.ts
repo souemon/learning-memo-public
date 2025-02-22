b36bedf7e7b978de92ab8464199ca41c256263ae
souemon <kobekobekitakita@gmail.com>
2025-02-22 15:47:19 +0900

    medium_06

---
+++ TypeScript/type-challenges/中級編/06_Chainable Options.ts
@@ -0,0 +1,10 @@
+// 出典：https://github.com/type-challenges/type-challenges/blob/main/questions/00012-medium-chainable-options/README.ja.md
+// 出典：https://zenn.dev/yskn_sid25/books/type-challenges/viewer/chapter2#chainable-options
+
+type Chainable<T = {}> = {
+  option<K extends string, V>(
+    key: K extends keyof T ? never : K,
+    value: V
+  ): Chainable<Omit<T, K> & Record<K, V>>;
+  get(): T;
+};
