79b17c9289b43e1461068875ed2a55558dabe1c7
souemon <kobekobekitakita@gmail.com>
2025-02-22 14:53:31 +0900

    medium_01

---
+++ TypeScript/type-challenges/中級編/01_Get Return Type.ts
@@ -0,0 +1,4 @@
+// 出典：https://github.com/type-challenges/type-challenges/blob/main/questions/00002-medium-return-type/README.ja.md
+// 出典：https://zenn.dev/yskn_sid25/books/type-challenges/viewer/chapter2#get-return-type
+
+type MyReturnType<T> = T extends (...args: any[]) => infer R ? R : never;
