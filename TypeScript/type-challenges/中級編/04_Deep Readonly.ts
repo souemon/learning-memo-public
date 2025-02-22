eda0798c68384b8c264963251026af4512c273df
souemon <kobekobekitakita@gmail.com>
2025-02-22 15:47:11 +0900

    medium_04

---
+++ TypeScript/type-challenges/中級編/04_Deep Readonly.ts
@@ -0,0 +1,10 @@
+// 出典：https://github.com/type-challenges/type-challenges/blob/main/questions/00009-medium-deep-readonly/README.ja.md
+// 出典：https://zenn.dev/yskn_sid25/books/type-challenges/viewer/chapter2#deep-readonly
+
+type DeepReadonly<T> = {
+  readonly [P in keyof T]: T[P] extends object
+    ? T[P] extends Function
+      ? T[P]
+      : DeepReadonly<T[P]>
+    : T[P];
+};
