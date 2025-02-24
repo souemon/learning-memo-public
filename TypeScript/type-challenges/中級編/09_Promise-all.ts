08abe80d2a6d0d0e9ed7e3f618b9c02c21a25243
souemon <kobekobekitakita@gmail.com>
2025-02-24 19:12:46 +0900

    medium_09

---
+++ TypeScript/type-challenges/中級編/09_Promise-all.ts
@@ -0,0 +1,9 @@
+// 出典：https://github.com/type-challenges/type-challenges/blob/main/questions/00020-medium-promise-all/README.ja.md
+// 出典：https://zenn.dev/yskn_sid25/books/type-challenges/viewer/chapter2#promise.all
+
+type MyAwaited<T> = T extends PromiseLike<infer U> ? MyAwaited<U> : T;
+declare function PromiseAll<T extends any[]>(
+  values: [...T]
+): Promise<{
+  [K in keyof T]: MyAwaited<T[K]>;
+}>;
