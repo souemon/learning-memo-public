5b0f81a7a370a6b275f25ddc6b0663e6d2777246
souemon <kobekobekitakita@gmail.com>
2025-02-25 20:58:14 +0900

    medium_11

---
+++ TypeScript/type-challenges/中級編/11_Trim Left.ts
@@ -0,0 +1,7 @@
+// 出典：https://github.com/type-challenges/type-challenges/blob/main/questions/00106-medium-trimleft/README.ja.md
+// 出典：https://zenn.dev/yskn_sid25/books/type-challenges/viewer/chapter3#trim-left
+
+type Space = " " | "\n" | "\t";
+type TrimLeft<S extends string> = S extends `${Space}${infer R}`
+  ? TrimLeft<R>
+  : S;
