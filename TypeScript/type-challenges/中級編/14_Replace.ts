919653b26f430d953af5ebe19682d329b7cec917
souemon <kobekobekitakita@gmail.com>
2025-02-25 20:58:28 +0900

    medium_14

---
+++ TypeScript/type-challenges/中級編/14_Replace.ts
@@ -0,0 +1,12 @@
+// 出典：https://github.com/type-challenges/type-challenges/blob/main/questions/00116-medium-replace/README.ja.md
+// 出典：https://zenn.dev/yskn_sid25/books/type-challenges/viewer/chapter3#replace
+
+type Replace<
+  S extends string,
+  From extends string,
+  To extends string
+> = From extends ""
+  ? S
+  : S extends `${infer L}${From}${infer R}`
+  ? `${L}${To}${R}`
+  : S;
