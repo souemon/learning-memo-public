afbe461793dcabe89110a99d73c17940670f15cc
souemon <kobekobekitakita@gmail.com>
2025-03-03 21:56:24 +0900

    easy_4-2

--- TypeScript/type-challenges/初級編/04_First of Array.ts
+++ TypeScript/type-challenges/初級編/04_First of Array.ts
@@ -1,6 +1,6 @@
 // 出典：https://github.com/type-challenges/type-challenges/blob/main/questions/00014-easy-first/README.ja.md
 // 出典：https://zenn.dev/yskn_sid25/books/type-challenges/viewer/chapter1#first-of-array
 
-type TupleToObject<T extends readonly PropertyKey[]> = {
-  [K in T[number]]: K;
-};
+type First<T extends any[]> = T extends [] ? never : T[0];
+
+// 2回目：NG
