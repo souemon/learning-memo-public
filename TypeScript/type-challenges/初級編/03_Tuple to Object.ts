d1261829bb7257ae4005a56adad5f38f886defd2
souemon <kobekobekitakita@gmail.com>
2025-03-03 21:56:19 +0900

    easy_3-2

--- TypeScript/type-challenges/初級編/03_Tuple to Object.ts
+++ TypeScript/type-challenges/初級編/03_Tuple to Object.ts
@@ -1,6 +1,12 @@
 // 出典：https://github.com/type-challenges/type-challenges/blob/main/questions/00011-easy-tuple-to-object/README.md
 // 出典：https://zenn.dev/yskn_sid25/books/type-challenges/viewer/chapter1#tuple-to-object
 
-type TupleToObject<T extends readonly PropertyKey[]> = {
+type TupleToObject1<T extends readonly (string | number | symbol)[]> = {
   [K in T[number]]: K;
 };
+
+type TupleToObject2<T extends readonly PropertyKey[]> = {
+  [K in T[number]]: K;
+};
+
+// 2回目：NG
